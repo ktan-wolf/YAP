0d3386ea30cf7277603c62d4bc7217794f4926a7
ktan-wolf <ktan247610@gmial.com>
2025-03-04 16:32:28 +0530

    cjors

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json({limit: "5mb"}));    
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: "yap-theta.vercel.app" ,
+app.use(cors({ origin: "https://yap-theta.vercel.app" ,
                 credentials : true, }));  // Adjust port if needed
 
 
