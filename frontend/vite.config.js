bde5444e05c602c62072492131ff1576b1dea327
ktan-wolf <ktan247610@gmial.com>
2025-03-05 13:35:05 +0530

    asdf

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    build: {
-      outDir: 'dist', // Change the output directory to "public"
-  },
     server: {
         proxy: {
           "/api": {
