f5cee2ba4c37c5e61151a2441df7171ab1e52f04
ktan-wolf <ktan247610@gmial.com>
2025-03-05 14:54:27 +0530

    asdfasdfasdf

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json({limit: "5mb"}));    
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: "https://yap-six.vercel.app" ,
+app.use(cors({ origin: ["https://yap-six.vercel.app" , "http://localhost:5173"] ,
                 credentials : true, }));  // Adjust port if needed
 
 
